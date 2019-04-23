ec63183ce7208aa843c798f79cf38cd0a218fc4b
austin vargason <vargasona@gmail.com>
2019-04-23 09:58:03 -0700

    added retrofit and hackathons

---
+++ app/src/main/java/com/example/acm_app_austinvargason/Event.java
@@ -0,0 +1,47 @@
+package com.example.acm_app_austinvargason;
+
+public class Event {
+    private String name;
+    private String url;
+    private String startDate;
+    private String endDate;
+    private String location;
+    private boolean isHighSchool;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Event(String name, String url, String startDate, String endDate, String location, boolean isHighSchool, String imageUrl) {
+        this.name = name;
+        this.url = url;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.isHighSchool = isHighSchool;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+
+        return imageUrl;
+    }
+
+}
--- app/src/main/java/com/example/acm_app_austinvargason/ResourcesFragment.java
+++ app/src/main/java/com/example/acm_app_austinvargason/ResourcesFragment.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -13,6 +21,9 @@ import android.view.ViewGroup;
  */
 public class ResourcesFragment extends Fragment {
 
+    //members
+    TextView results;
+    List<Event> hackathons;
 
     public ResourcesFragment() {
         // Required empty public constructor
@@ -22,8 +33,61 @@ public class ResourcesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resources, container, false);
+        //Understanding this completely is out of the scope of the workshop
+        //Just know that we are getting our root view since we are using a fragment instead of an activity.
+        //We must know what activity we are on.
+        View rootView = inflater.inflate(R.layout.fragment_resources, container, false);
+
+        //Find the results TextView in our XML through Id
+        results = rootView.findViewById(R.id.results);
+
+        //LoadJSON is a custom method, we take in the parameter container since this is a fragment.
+        LoadJson(container);
+
+        return rootView;
     }
+
+
+    public void LoadJson(final ViewGroup container){
+        MLHInterface apiInterface = APIClient.getApiClient().create(MLHInterface.class);
+
+        Call<List<Event>> call;
+        call = apiInterface.getEvents();
+
+        call.enqueue(new Callback<List<Event>>() {
+            @Override
+            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                hackathons = filterEvents(response.body());
+                for (Event event : hackathons) {
+                    String content = "";
+                    content += "Name: " + event.getName() + "\n";
+                    content += "Location: " + event.getLocation() + "\n";
+                    content += "Start Date: " + event.getStartDate() + "\n";
+                    content += "End Date: " + event.getEndDate() + "\n";
+                    content += "URL: " + event.getUrl() + "\n\n";
+
+                    results.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Event>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public List<Event> filterEvents(final List<Event> allEvents){
+        List<Event> caEvents = new ArrayList<>();
+        for(int i = allEvents.size() - 1; i > 0; i--){
+            //Filter California Events
+            if(allEvents.get(i).getLocation().contains("CA")){
+                caEvents.add(allEvents.get(i));
+            }
+        }
+        return caEvents;
+    }
+
+
 
 }
